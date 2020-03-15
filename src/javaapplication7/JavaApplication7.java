de14d2a597f00458cc8295e7cf22dbb7fa6bd3ff
EllairaT <EllairaT@users.noreply.github.com>
2020-03-16 12:23:55 +1300

    test from laptop

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -10,6 +10,7 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         System.out.println("main!");
     }
     
